eef924ef8044541d16586580eaaca0aad7a195d8
Heitor Polidoro <heitor.polidoro@gmail.com>
2025-12-15 10:43:33 -0300

    chore: remove outdated and unused configuration files, tests, and modules
    - Deleted obsolete Credo configuration and formatter settings.
    - Removed unused test files, LiveView modules, and associated UI components.
    - Cleaned up Docker ignore rules and eliminated deprecated parsing modules.

--- test/cash_lens_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule CashLensWeb.PageControllerTest do
-  use CashLensWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
