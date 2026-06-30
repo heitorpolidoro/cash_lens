a85423d4c41f90535e12d1dae1bda42e417ab204
Heitor Polidoro <heitor.polidoro@gmail.com>
2026-06-30 13:45:54 -0300

    feat(categories): seed Cartão de Crédito category

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -3,3 +3,4 @@ alias CashLens.Categories.Category
 
 Repo.insert!(%Category{name: "Valor Inicial", slug: "initial_value"}, on_conflict: :nothing)
 Repo.insert!(%Category{name: "Transferência", slug: "transfer"}, on_conflict: :nothing)
+Repo.insert!(%Category{name: "Cartão de Crédito", slug: "cartao-de-credito"}, on_conflict: :nothing)
